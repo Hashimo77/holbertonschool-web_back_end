cdabdb5c938b8dab98a68deb6478b91f2594979d
Hashimo77 <hagamirz7@gmail.com>
2025-12-12 13:06:30 +0000

    sh

---
+++ python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Type-annotated function make_multiplier
+"""
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """
+    Returns a function that multiplies a float by multiplier.
+    """
+    def func(n: float) -> float:
+        return n * multiplier
+    return func
