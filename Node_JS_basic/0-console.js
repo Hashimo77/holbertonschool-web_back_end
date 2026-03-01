dad13a8344096a579d71a3f23424aa26d3bb66b4
Hashimo77 <hagamirz7@gmail.com>
2026-03-01 08:20:34 +0000

    sg

---
+++ Node_JS_basic/0-console.js
@@ -0,0 +1,9 @@
+/**
+ * Simple function to print a message to the standard output.
+ * @param {string} message - The string to be printed.
+ */
+const displayMessage = (message) => {
+  console.log(message);
+};
+
+module.exports = displayMessage;
